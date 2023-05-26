793fd5ca7ba5874138d14abdf0d8e276d0a1f151
unknown <C:\Users\\AppData\Roaming\The Bat!>
2023-05-26 13:50:50 +0500

    add

--- app.js
+++ app.js
@@ -35,30 +35,124 @@ const cardBlockswiper = new Swiper(cardBlock, {
   },
 });
 
-//btn-sound
-const slideContent = document.querySelectorAll('.card-block__slide');
+// //btn-sound
+// const slideContent = document.querySelectorAll('.card-block__slide');
 
-const slideChangeFunc = () => {
-  cardBlockswiper.on('slideChange', () => {
-    const sound = document.querySelectorAll('.card-block__audio');
-    sound.forEach((item) => {
-      item.classList.remove('play');
-      item.pause();
-    });
-  });
+// const slideChangeFunc = () => {
+//   cardBlockswiper.on('slideChange', () => {
+//     const sound = document.querySelectorAll('.card-block__audio');
+//     sound.forEach((item) => {
+//       item.classList.remove('play');
+//       item.pause();
+//     });
+//   });
+// };
+
+// slideChangeFunc();
+
+// const soundBtnFunc = () => {
+//   slideContent.forEach((item) => {
+//     const soundBtn = item.querySelector('.card-block__btn-sound');
+//     soundBtn.addEventListener('click', () => {
+//       const sound = item.querySelector('.card-block__audio');
+//       sound.classList.add('play');
+//       sound.play();
+//     });
+//   });
+// };
+
+// soundBtnFunc();
+
+//player
+const player = document.querySelector('.card-block__player');
+const playBtn = document.querySelector('.card-block__player-btn-play');
+const prevBtn = document.querySelector('.card-block__player-btn-prev');
+const nextBtn = document.querySelector('.card-block__player-btn-next');
+const audio = document.querySelector('.card-block__player-audio');
+const progressContainer = document.querySelector('.card-block__player-progress-container');
+const progress = document.querySelector('.card-block__player-progress');
+const title = document.querySelector('.card-block__player-song');
+const imgSrc = document.querySelector('.img__src');
+
+const songs = ['song-1', 'song-2', 'song-3', 'song-4'];
+
+//default song
+let songIndex = 0;
+
+//Init
+const loadSong = (song) => {
+  title.innerHTML = song;
+  audio.src = `sounds/${song}.mp3`;
 };
 
-slideChangeFunc();
+loadSong(songs[songIndex]);
 
-const soundBtnFunc = () => {
-  slideContent.forEach((item) => {
-    const soundBtn = item.querySelector('.card-block__btn-sound');
-    soundBtn.addEventListener('click', () => {
-      const sound = item.querySelector('.card-block__audio');
-      sound.classList.add('play');
-      sound.play();
-    });
-  });
+//play
+const playSong = () => {
+  player.classList.add('play');
+  imgSrc.src = 'img/pause.svg';
+  audio.play();
 };
 
-soundBtnFunc();
+//pause
+const pauseSong = () => {
+  player.classList.remove('play');
+  imgSrc.src = 'img/play.svg';
+  audio.pause();
+};
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = player.classList.contains('play');
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+//nextSong, prevSong
+const nextSong = () => {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+const prevSong = () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener('click', prevSong);
+
+//progressBar
+
+const updateProgress = (e) => {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercents = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercents}%`;
+};
+
+audio.addEventListener('timeupdate', updateProgress);
+
+//setProgress
+
+const setProgress = (e) => {
+  const width = progressContainer.offsetWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+};
+
+progressContainer.addEventListener('click', setProgress);
+
+//autoplay
+audio.addEventListener('ended', nextSong);
